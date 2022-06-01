b3190311a5eedd6b9228d34831756c4c0fb4b6bb
ChiriacCatalin <catalin.chiriac00@gmail.com>
2022-06-01 14:43:17 +0300

    checked validity of youtube url

--- util/funtions.js
+++ util/funtions.js
@@ -1,21 +1,26 @@
 function getVideoId(url) {
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-  const match = url.match(regExp);
-  return match && match[2].length === 11 ? match[2] : null;
+  var p =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  if (url.match(p)) {
+    return url.match(p)[1];
+  }
+  return false;
 }
 
-function findVideoFields(obj){
+function findVideoFields(obj) {
   Object.keys(obj).forEach((key) => {
     if (key.includes("Video") && obj[key] !== null) {
-      obj[key] = "https://www.youtube.com/embed/" + getVideoId(obj[key]);
+      obj[key] = getVideoId(obj[key])
+        ? "https://www.youtube.com/embed/" + getVideoId(obj[key])
+        : null;
     }
     if (typeof obj[key] === "object" && obj[key] !== null) {
       findVideoFields(obj[key]);
     }
   });
-};
+}
 
 // modify the standard yotube video URL so that it can be embeded
 exports.modifyVideoUrl = (obj) => {
-    findVideoFields(obj);
-}
+  findVideoFields(obj);
+};
